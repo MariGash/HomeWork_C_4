9b4190b8c607a3063704445f8533f26bfcfcdd94
Maria <mgashenina@gmail.com>
2022-12-12 22:13:09 +0300

    homework_4_1

---
+++ homework_4_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int P = 1;
+while (i < B)
+{
+    P = P * A;
+    i++;
+}
+Console.WriteLine(A + " в степени " + B + " равно " + P);
